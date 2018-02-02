87e8578083bc37cf0a8082fc036c9eed19cf9f70
WangGi Lim <wanggilim@gmail.com>
2018-02-02 12:40:43 +0900

    요일 (DP) 미선택시 앱 다운 예외 발생 해결.

--- app/src/main/java/project/wgl/callarm/DayPDialogPreference.java
+++ app/src/main/java/project/wgl/callarm/DayPDialogPreference.java
@@ -45,7 +45,6 @@ public class DayPDialogPreference extends MultiSelectListPreference {
     protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
         setEntries(context.getResources().getStringArray(R.array.ddd));
         setEntryValues(context.getResources().getStringArray(R.array.ddd_result));
-
         super.onPrepareDialogBuilder(builder);
     }
 
@@ -57,34 +56,46 @@ public class DayPDialogPreference extends MultiSelectListPreference {
         if (positiveResult) {
             Set<String> getDaySet = getSharedPreferences().getStringSet(this.getKey(), null);
             //Set<String> getDaySet = sharedPreferences.getStringSet(key, new HashSet<String>());//도 됨!
-            Iterator<String> dayIter = getDaySet.iterator();
-            String day = "";
 
-            // int형 ArrayList로 형변환
-            ArrayList<Integer> i_days = new ArrayList<Integer>();
-            for (String s1 : getDaySet) {
-                i_days.add(Integer.valueOf(s1));
+            if (getDaySet == null || getDaySet.size() == 0) {
+                /**
+                 * TODO
+                 * (충분한 테스트) + strings
+                 */
+                setSummary("요일을 설정합니다");
+                Log.d(TAG, "onDialogClosed: setSummary Init");
+            } else {
+                Iterator<String> dayIter = getDaySet.iterator();
+                String day = "";
+
+                // int형 ArrayList로 형변환
+                ArrayList<Integer> i_days = new ArrayList<Integer>();
+                for (String s1 : getDaySet) {
+                    i_days.add(Integer.valueOf(s1));
+                }
+
+                // int형 배열로 변환 + 정렬
+                int[] sort = new int[i_days.size()];
+                for (int i = 0; i < sort.length; i++) {
+                    sort[i] = i_days.get(i).intValue();
+                    Log.d(TAG, "onDialogClosed: sort[" + i + "] " + sort[i]);
+                }
+
+                Arrays.sort(sort);
+
+                for (int i = 0; i < sort.length; i++) {
+                    day += getEntries()[sort[i]] + " ";
+                    Log.d(TAG, "onDialogClosed: day 목록 => " + day);
+                }
+
+                String prefDay = "매주 ";
+                String postDay = "마다";
+                setSummary(
+                        prefDay + day + postDay
+                );
+
             }
 
-            // int형 배열로 변환 + 정렬
-            int[] sort = new int[i_days.size()];
-            for (int i = 0; i < sort.length; i++) {
-                sort[i] = i_days.get(i).intValue();
-                Log.d(TAG, "onDialogClosed: sort["+i+"] " + sort[i] );
-            }
-
-            Arrays.sort(sort);
-
-            for (int i = 0; i < sort.length; i++) {
-                day += getEntries()[sort[i]] + " ";
-                Log.d(TAG, "onDialogClosed: day 목록 => " + day);
-            }
-
-            String prefDay = "매주 ";
-            String postDay = "마다";
-            setSummary(
-                    prefDay + day + postDay
-            );
             Log.d(TAG, "onDialogClosed: 완료");
         }
     }
