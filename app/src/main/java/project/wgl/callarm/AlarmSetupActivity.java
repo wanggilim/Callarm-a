5d61251574f5464a8e9488ee3b49f99662c864a4
WangGi Lim <wanggilim@gmail.com>
2018-02-08 17:00:21 +0900

    알람 설정 화면 내부로 Toolbar 추가

--- app/src/main/java/project/wgl/callarm/AlarmSetupActivity.java
+++ app/src/main/java/project/wgl/callarm/AlarmSetupActivity.java
@@ -1,26 +1,46 @@
 package project.wgl.callarm;
 
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.support.annotation.Nullable;
-import android.util.AttributeSet;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by WGL on 2018. 1. 10..
  */
 
-public class AlarmSetupActivity extends PreferenceActivity {
+public class AlarmSetupActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
     private static final String TAG = "AlarmSetupActivity";
+
+    private AlarmSetupHolder holder;
+
+
+    public AlarmSetupActivity() {
+        holder = new AlarmSetupHolder();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_setup);
+
+        holder.toolbar = findViewById(R.id.tb_setup_alarm);
+        holder.toolbar.inflateMenu(R.menu.set_alarm);
+        holder.toolbar.setOnMenuItemClickListener(this);
+        /**
+         * TODO
+         * strings
+         */
+        holder.toolbar.setTitle("알람 설정");
+        holder.toolbar.setTitleTextColor(Color.WHITE);
+
+        getSupportActionBar().hide(); // 기존에 있던 액션바 숨기기
 
         getFragmentManager().beginTransaction()
                 .addToBackStack("AlarmSetupFragment")
@@ -40,4 +60,26 @@ public class AlarmSetupActivity extends PreferenceActivity {
         Log.d(TAG, "onBackPressed: ");
         super.onBackPressed();
     }
+
+    //////////////////////////////////////////////////
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        /**
+         * TODO
+         * strings
+         */
+        switch (item.getItemId()) {
+            case R.id.item_save_alarm:
+                Toast.makeText(getBaseContext(), "까꿍!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+}
+
+class AlarmSetupHolder {
+    Toolbar toolbar;
 }
