8afcaabd02b0d8855026911f4e329e4a4e156c0b
WGL <wanggilim@gmail.com>
2018-05-17 21:16:18 +0900

    상수 파일, 기본값 파일 추가

---
+++ app/src/main/java/project/wgl/callarm/Constants.java
@@ -0,0 +1,10 @@
+package project.wgl.callarm;
+
+/**
+ * 상수
+ * WGL 2018. 5. 17
+ */
+public class Constants {
+
+    public final static String TAG = "Constants";
+}
---
+++ app/src/main/java/project/wgl/callarm/DefaultValues.java
@@ -0,0 +1,10 @@
+package project.wgl.callarm;
+
+/**
+ * 기본값
+ * WGL 2018. 5. 17
+ */
+public class DefaultValues {
+
+    public final static String TAG = "DefaultValues";
+}
